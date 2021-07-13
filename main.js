d3be37938171c46a191d3af306077988e6ff5fd6
Puran06 <purandahal68@gmail.com>
2021-07-13 23:39:48 +0545

    test commit

---
+++ main.js
@@ -0,0 +1,61 @@
+//document.write('hello world!');
+
+// lets begain javascript 
+
+
+//object in javascript
+/*let student = {
+    first : 'puran',
+    last : 'dahal',
+    age : 19,
+    height : 7.5,
+studentInfo : function(){
+    return this.first +'\n' + this.last +'\n' + this.age +'\n' + this.height
+}
+} ;
+
+student.age++;
+document.write(student.studentInfo());
+document.write("</br>");
+document.write( 'puran age :' +'\n' + student.age);*/
+
+
+//if else statement
+
+/*let name = prompt('please enter your name');
+if (name == 'dipsha' ){
+    document.write('your are my life'+ ' ' +name);
+}
+else{ (name === 'dipsha') 
+    document.write(' sry! i have beautiful, loving, caring girlfriend');
+}*/
+
+
+
+//i just want to prompt the numberrs if my prompt number sunday monday tuesday or wednesday or
+
+/*let y; 
+y= prompt("choose a number between 1 and 7");
+switch (y) {
+    case "1":
+        call == 'good day';
+        
+        break;
+        case "3":
+        call = 'weekend';
+        
+        break;
+        case "7":
+        call = 'nervous day ';
+        
+        break;
+
+    default :
+        call = 'bad day'
+        break;
+}
+
+document.write(call);*/
+
+
+
